132341c3db9bcad9582014746f068922e04c6e9c
Gen <genesishawkeye42@gmail.com>
2022-11-21 18:35:02 -0300

    fixed the strange behavior on clicking the navbar

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
-const navigationItems = document.querySelectorAll(".navigation__list li");
-
-console.log(navigationItems);
+const navigationItems = document.querySelectorAll(".navigation__list li a");
 
 navigationItems.forEach((item) => {
   item.addEventListener("click", () => {
     removeActive();
-    item.classList.add("active");
+    item.parentElement.classList.add("active");
   });
 });
 
 function removeActive() {
-  navigationItems.forEach((item) => {
-    if (item.classList.contains("active")) {
-      item.classList.remove("active");
-    }
-  });
+  const currentActive = document.querySelector(".navigation li.active");
+  if (currentActive) {
+    currentActive.classList.remove("active");
+  }
 }
+
+const menuBtn = document.querySelector("#header-menu-button");
+const backdrop = document.querySelector(".backdrop");
+const nav = document.querySelector(".navigation");
+
+menuBtn.addEventListener("click", () => {
+  nav.classList.add("active");
+  backdrop.classList.add("active");
+});
+
+backdrop.addEventListener("click", () => {
+  nav.classList.remove("active");
+  backdrop.classList.remove("active");
+});
