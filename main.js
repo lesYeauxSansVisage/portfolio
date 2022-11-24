7778882bfe0328031c5a6085e18bbffd21a11039
Gen <genesishawkeye42@gmail.com>
2022-11-23 22:54:31 -0300

    added the javascript to add active to nav when scrolling

--- main.js
+++ main.js
@@ -27,3 +27,43 @@ backdrop.addEventListener("click", () => {
   nav.classList.remove("active");
   backdrop.classList.remove("active");
 });
+
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", fixNav);
+
+function fixNav() {
+  if (window.scrollY > header.offsetHeight + 84) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+}
+
+const sections = document.querySelectorAll("section[id]");
+
+// Add an event listener listening for scroll
+window.addEventListener("scroll", navHighlighter);
+
+function navHighlighter() {
+  let scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+
+    const sectionTop =
+      current.getBoundingClientRect().top + window.pageYOffset - 80;
+    const sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      removeActive();
+      document
+        .querySelector(".navigation ul li a[href*=" + sectionId + "]")
+        .parentElement.classList.add("active");
+    } else {
+      document
+        .querySelector(".navigation a[href*=" + sectionId + "]")
+        .parentElement.classList.remove("active");
+    }
+  });
+}
