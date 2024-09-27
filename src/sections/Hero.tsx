b9e9c13987da014df52cf3ac60f74869b6cbeffa
Gen <genesishawkeye42@gmail.com>
2024-09-27 11:08:36 -0300

    FEAT: Add Hero section

---
+++ src/sections/Hero.tsx
@@ -0,0 +1,15 @@
+import "./Hero.scss";
+
+const Hero = () => {
+  return (
+    <section className={"hero"}>
+      <h1 className={"heading-primary"}>
+        <span className={"heading-primary-main"}>Gilberto</span>
+
+        <span className={"heading-primary-sub"}>Desenvolvedor Front-end</span>
+      </h1>
+    </section>
+  );
+};
+
+export default Hero;
