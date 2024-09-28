0e08dbfff22a14fbf07e0dee1a3655db132398eb
Gen <genesishawkeye42@gmail.com>
2024-09-28 11:23:26 -0300

    FEAT: Changed hero text

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -2,11 +2,12 @@ import "./Hero.scss";
 
 const Hero = () => {
   return (
-    <section className={"hero"}>
-      <h1 className={"heading-primary"}>
-        <span className={"heading-primary-main"}>Gilberto</span>
-
-        <span className={"heading-primary-sub"}>Desenvolvedor Front-end</span>
+    <section className="hero">
+      <h1 className="heading-primary">
+        <span className="heading-primary-main">
+          Olá. Eu sou<span>&nbsp;&lt;Gilberto &#8725;&gt;</span>.
+        </span>
+        <span className="heading-primary-sub">Desenvolvedor Front-end</span>
       </h1>
     </section>
   );
