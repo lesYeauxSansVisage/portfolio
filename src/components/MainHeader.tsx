bf607d28ef616c6f54b18e29bea02661f8269c6f
Gen <genesishawkeye42@gmail.com>
2024-09-30 11:28:14 -0300

    FEAT: Add background color change on scroll

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -1,21 +1,47 @@
+import { useEffect, useState } from "react";
 import "./MainHeader.scss";
 
+const headerLinks = [
+  { name: "Sobre", href: "#about" },
+  { name: "Projetos", href: "#projects" },
+];
+
 const MainHeader = () => {
+  const [navbarActive, setNavbarActive] = useState(false);
+
+  useEffect(() => {
+    const changeBackground = () => {
+      if (window.scrollY >= 60) {
+        setNavbarActive(true);
+      } else {
+        setNavbarActive(false);
+      }
+    };
+    window.addEventListener("scroll", changeBackground);
+
+    return () => {
+      window.removeEventListener("scroll", changeBackground);
+    };
+  }, []);
+
+  const mainHeaderClassname = navbarActive
+    ? "main-header active"
+    : "main-header";
+
   return (
-    <header className={"main-header"}>
-      <div className={"logo"}>
-        <a href="/">
+    <header className={mainHeaderClassname}>
+      <div className="main-header__logo">
+        <a href="#">
           <img src="\portfolio\icons8-letter-g-250.png" alt="" />
         </a>
       </div>
-      <nav className={"main-navigation"}>
+      <nav className="main-navigation">
         <ul>
-          <li>
-            <a href="/projects">Projetos</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
+          {headerLinks.map((link) => (
+            <li key={link.href}>
+              <a href={link.href}>{link.name}</a>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
