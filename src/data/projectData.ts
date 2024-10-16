2a8d700d39870a5b928864c51571021924c8f800
Gen <genesishawkeye42@gmail.com>
2024-10-16 16:44:23 -0300

    FEAT: Add projectData array to get the data needed in ProjectsList

---
+++ src/data/projectData.ts
@@ -0,0 +1,34 @@
+export const projectsData = [
+  {
+    title: "Teste Agile Content",
+    description:
+      "Teste para Desenvolvedor Front-end da Agile Content. Feito em React, usando SASS com a estrutura 7-1.",
+    github: "https://github.com/lesYeauxSansVisage/Agile-FrontEnd-Test",
+    live: "https://lesyeauxsansvisage.github.io/Agile-FrontEnd-Test/",
+    image: "/portfolio/agile-test.png",
+  },
+  {
+    title: "E-Card",
+    description:
+      "Jogo de cartas baseado em uma animação. Feito usando Angular para a interface e Karma/Jasmine para os testes.",
+    github: "https://github.com/lesYeauxSansVisage/e-card",
+    live: "https://lesyeauxsansvisage.github.io/e-card/",
+    image: "/portfolio/e-card.jpg",
+  },
+  {
+    title: "Projeto Final - SDC Academy",
+    description:
+      "Limpeza e unificação de dados vindos de diferentes fontes carregados no Power BI, produzindo também a documentação e um Dashboard para visualização.",
+    github: "https://github.com/lesYeauxSansVisage/SDC-Academy-Sales-Info",
+    live: null,
+    image: "/portfolio/sdc-projeto-final.jpg",
+  },
+  {
+    title: "React Weather App",
+    description:
+      "Aplicação de clima feita em React baseado em um teste para uma vaga de Desenvolvedor Front-end. Contém testes usando vitest.",
+    github: "https://github.com/lesYeauxSansVisage/react-weather-app",
+    live: "https://lesyeauxsansvisage.github.io/react-weather-app/",
+    image: "/portfolio/weather-app.jpg",
+  },
+];
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,15 +1,13 @@
-import { projectData } from "../components/Project";
 import ProjectsList from "../components/ProjectsList";
 import "./Projects.scss";
-
-const dataArray = [projectData, projectData, projectData, projectData];
+import { projectsData } from "../data/projectData";
 
 const Projects = () => {
   return (
     <section id="projects" className="projects-section">
       <h2 className="section-title section-title--dark">projetos</h2>
 
-      <ProjectsList projects={dataArray} />
+      <ProjectsList projects={projectsData} />
     </section>
   );
 };
