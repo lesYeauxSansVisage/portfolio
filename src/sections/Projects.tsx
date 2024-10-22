5ca0ed55d8aff9727f6b15e7136fc0cf16c166ca
Gen <genesishawkeye42@gmail.com>
2024-10-22 11:10:04 -0300

    FEAT: Add animation to h2

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -4,10 +4,18 @@ import ProjectsList from "../components/ProjectsList";
 
 import { projectsData } from "../data/projectData";
 
+import { motion } from "framer-motion";
+import { slideInFromLeft } from "../framer-animations/slideIn";
+
 const Projects = () => {
   return (
     <section id="projects" className="projects-section">
-      <h2 className="section-title section-title--dark">projetos</h2>
+      <motion.h2
+        className="section-title section-title--dark"
+        {...slideInFromLeft}
+      >
+        projetos
+      </motion.h2>
 
       <ProjectsList projects={projectsData} />
     </section>
